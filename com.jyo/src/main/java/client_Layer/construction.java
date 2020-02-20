4f31753248f33233cb4e46c753b5edf0912bb519
AvvariJyothi26 <60391779+AvvariJyothi26@users.noreply.github.com>
2020-02-20 22:53:06 +0530

    Add files via upload

---
+++ com.jyo/src/main/java/client_Layer/construction.java
@@ -0,0 +1,5 @@
+package client_Layer;
+
+public interface construction {
+	String Constructions_Materials(String Material_Standard,int Area );
+}
---
+++ com.jyo/src/main/java/pre_Layer/presented.java
@@ -0,0 +1,6 @@
+package pre_Layer;
+
+public interface presented {
+	int Compound_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+	int Simple_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+}
